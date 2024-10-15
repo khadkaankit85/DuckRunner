e21b9a090b30df9ff30705e74fc1d955f05f5b7d
Angkit Khadka <khadkaankit85@gmail.com>
2024-10-14 21:36:37 -0400

    config chnge

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
 });
+
+// for dev
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
+
+// export default defineConfig({
+//   plugins: [react(), basicSsl()],
+// });
