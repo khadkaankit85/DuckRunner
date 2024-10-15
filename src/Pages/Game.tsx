392982ecf8f5b1f39c38824e92fbd28f4a107ff4
Angkit Khadka <khadkaankit85@gmail.com>
2024-10-15 00:03:39 -0400

    Updates

--- src/Pages/Game.tsx
+++
@@ -1,7 +0,0 @@
-// import Listen from "../Components/Listen";
-
-const Game = () => {
-  return <div className=" w-full h-screen "></div>;
-};
-
-export default Game;
--- src/utils/getRandomImage.ts
+++
@@ -1,6 +0,0 @@
-import { obstacles } from "../Data/appdata";
-
-export const getRandomImg = () => {
-  const randomI = Math.floor(Math.random() * (17 - 0) + 0);
-  return obstacles[randomI];
-};
